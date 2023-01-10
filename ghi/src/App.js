aa482ea8ed6a6b0a8b8b87508c36552ae261f011
Triston <btdewayne28@gmail.com>
2023-01-10 11:44:57 -0600

    Cleaning for merging

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,27 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import ListDogs from './dogs/ListDogs';
-import './App.css';
-import LoginForm from './LoginForm';
-import SignUpForm from './SignUpForm';
-import { AuthProvider, useToken } from "./token";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ListDogs from './dogs/ListDogs'
+import Nav from './Nav'
+import './App.css'
+import LoginForm from './LoginForm'
+import SignUpForm from './SignUpForm'
+import { AuthProvider, useToken } from "./token"
 
 function GetToken() {
-    useToken();
+    useToken()
     return null
 }
 
 function App() {
-  const [token, login, logout, signup, update] = useToken();
+  const [token, login, logout, signup, update] = useToken()
   return (
     <AuthProvider>
       <GetToken/>
+      <Nav />
+      <div className='container'>
         <Routes>
-          {/* <Route path='/dogs' element={<ListDogs />} /> */}
+          <Route path='/dogs' element={<ListDogs />} />
           {<Route path='/login' element={<LoginForm token={token} login={login}/>} />}
           {<Route path='/signup' element={<SignUpForm token={token} signup={signup}/>} />}
         </Routes>
+        </div>
     </AuthProvider>
   )
 }
 
-export default App;
+export default App
